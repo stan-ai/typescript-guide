828d73032c714ead1326a977916c5cbff65d6c19
Prem Raval <50078718+premraval1710@users.noreply.github.com>
2021-02-19 12:08:52 -0500

    Compiler Commit

--- data-types/function-types.ts
+++ data-types/function-types.ts
@@ -1,22 +1,22 @@
 // The :number in the last after parameters is a return type
 
-function add (n1: number, n2: number): number{
+function addNumber (n1: number, n2: number): number{
     return n1 + n2;
 }
 
-function printResult(num: number): void{
+function printResultVoid(num: number): void{
     console.log("Result: " + num);
 
     // return 1; // ERROR, the return type is void, so you can't return stuff. Good Check
 }
 
-printResult(add(2,5));
+printResultVoid(addNumber(2,5));
 
 // Function Type: A type for a function
 
 let addResult: (a: number, b: number) => number;
 
-addResult = add;
+addResult = addNumber;
 // addResult = printResult; // ERROR, only Function with defined type accepted
 // addResult = 5 // ERROR, only functions accepted
 
--- js-only.js
+++
@@ -1,3 +0,0 @@
-const add = (num1, num2) => num1 + num2;
-
-console.log(add("10", "5"));
